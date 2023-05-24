180e3a01677bc2e9e014288756fc9fdd988e0b76
Rennan Bacili <rennanbaccili@gmail.com>
2023-05-24 09:13:33 -0300

    Programa salvo

---
+++ src/model/entities/Vehicle.java
@@ -0,0 +1,28 @@
+package model.entities;
+
+public class Vehicle {
+	private String vehicle;
+
+	
+	
+	public Vehicle() {
+		super();
+	}
+
+
+	public Vehicle(String vehicle) {
+		super();
+		this.vehicle = vehicle;
+	}
+
+
+	public String getVehicle () {
+		return vehicle;
+	}
+
+
+	public void setVehicle(String vehicle ) {
+		this.vehicle = vehicle;
+	}
+	
+}
